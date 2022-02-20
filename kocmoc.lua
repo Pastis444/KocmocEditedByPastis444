579b1f27caef730b74ff6712d59459557cce6e20
unknown <yanissansnom444@gmail.com>
2022-02-20 15:44:20 +0100

    init

--- kocmoc.lua
+++ kocmoc.lua
@@ -122,12 +122,14 @@ local temptable = {
     local tab = game.ReplicatedStorage.Events.RetrievePlayerStats:InvokeServer(),
     oldtool = rtsg()["EquippedCollector"],
     oldmaskTab = tab["EquippedAccessories"],
-    oldmask = oldmaskTab["Hat"],
+    --oldmask = oldmaskTab["Hat"],
     ['gacf'] = function(part, st)
         coordd = CFrame.new(part.Position.X, part.Position.Y+st, part.Position.Z)
         return coordd
     end
 }
+print(temptable.oldmaskTab)
+
 local planterst = {
     plantername = {},
     planterid = {}
