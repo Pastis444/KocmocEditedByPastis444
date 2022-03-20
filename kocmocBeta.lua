277777a64c184808a28ad6ac89c5a17726a5d64c
Pastis444 <yanissansnom444@gmail.com>
2022-03-20 21:52:45 +0100

    Improvement

--- kocmocBeta.lua
+++ kocmocBeta.lua
@@ -1092,7 +1092,7 @@ end end)
 
 game.Workspace.Particles.ChildAdded:Connect(function(v)
     if not temptable.started.vicious and not temptable.started.ant then
-        if v.Name == "WarningDisk" and not temptable.started.vicious and kocmoc.toggles.autofarm and not temptable.started.ant and kocmoc.toggles.farmcoco and (v.Position-api.humanoidrootpart().Position).magnitude < temptable.magnitude and not temptable.converting and v.BrickColor ~= BrickColor.new("Really red") then
+        if v.Name == "WarningDisk" and v ~= nil and v.BrickColor ~= BrickColor.new("Really red") and not temptable.started.vicious and kocmoc.toggles.autofarm and not temptable.started.ant and kocmoc.toggles.farmcoco and (v.Position-api.humanoidrootpart().Position).magnitude < temptable.magnitude and not temptable.converting then
             table.insert(temptable.coconuts, v)
             getcoco(v)
             gettoken()
