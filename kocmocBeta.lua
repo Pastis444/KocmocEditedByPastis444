0b81266cdf5941f0bf8d82df09c42e8d399758d4
Pastis444 <yanissansnom444@gmail.com>
2022-03-20 19:47:24 +0100

    Fix Honey Wreath

--- kocmocBeta.lua
+++ kocmocBeta.lua
@@ -1542,7 +1542,7 @@ task.spawn(function() while task.wait(1) do
     if kocmoc.toggles.freeantpass then game:GetService("ReplicatedStorage").Events.ToyEvent:FireServer("Free Ant Pass Dispenser") end
     gainedhoneylabel:UpdateText("Gained Honey: "..api.suffixstring(temptable.honeycurrent - temptable.honeystart))
     elapsetime:UpdateText("Time Elapse: "..os.date('%X', os.time() - temptable.starttime))
-    print(os.time() - temptable.starttime)
+    
     if kocmoc.toggles.autocloudvial then 
         local stats = game:GetService("ReplicatedStorage").Events.RetrievePlayerStats:InvokeServer()
         local lastWindy = stats.SystemTimes.WindShrine
